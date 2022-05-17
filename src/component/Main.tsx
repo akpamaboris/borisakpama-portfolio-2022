bb46ecf80313a0a5744ae7e1657d2573c36d0a28
Zeliwipin <zeliwipin@gmail.com>
2022-05-17 21:37:04 +0200

    added some changement

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -10,70 +10,69 @@ const Main = () => {
       >
         <div className="max-w-4xl mx-auto mt-16" style={{ cursor: "auto" }}>
           <p
-            className="text-2xl md:text-4xl font-bold text-center"
+            className="text-2xl font-bold text-center md:text-4xl"
             style={{ cursor: "auto" }}
           >
             My Background
           </p>
           <p
-            className="text-base text-left md:text-center text-gray-600 leading-relaxed mt-4"
+            className="mt-4 text-base leading-relaxed text-left text-gray-600 md:text-center"
             style={{ cursor: "auto" }}
           >
-            I am a working developper since 2021, but have worked a few year
-            with technology before, just non professionally.
+            Passionate working developer since 2021.
           </p>
         </div>
         <div
-          className="flex flex-col sm:flex-row align-center justify-center max-w-2xl mx-auto mt-8"
+          className="flex flex-col justify-center max-w-2xl mx-auto mt-8 sm:flex-row align-center"
           style={{ cursor: "auto" }}
         >
-          <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
+          <div className="flex-none w-40 p-5 m-4 mx-auto text-center border-2 border-gray-300 rounded-xl">
             <svg
               stroke="currentColor"
               fill="currentColor"
               stroke-width="0"
               viewBox="0 0 640 512"
-              className="text-4xl mx-auto inline-block"
+              className="inline-block mx-auto text-4xl"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M278.9 511.5l-61-17.7c-6.4-1.8-10-8.5-8.2-14.9L346.2 8.7c1.8-6.4 8.5-10 14.9-8.2l61 17.7c6.4 1.8 10 8.5 8.2 14.9L293.8 503.3c-1.9 6.4-8.5 10.1-14.9 8.2zm-114-112.2l43.5-46.4c4.6-4.9 4.3-12.7-.8-17.2L117 256l90.6-79.7c5.1-4.5 5.5-12.3.8-17.2l-43.5-46.4c-4.5-4.8-12.1-5.1-17-.5L3.8 247.2c-5.1 4.7-5.1 12.8 0 17.5l144.1 135.1c4.9 4.6 12.5 4.4 17-.5zm327.2.6l144.1-135.1c5.1-4.7 5.1-12.8 0-17.5L492.1 112.1c-4.8-4.5-12.4-4.3-17 .5L431.6 159c-4.6 4.9-4.3 12.7.8 17.2L523 256l-90.6 79.7c-5.1 4.5-5.5 12.3-.8 17.2l43.5 46.4c4.5 4.9 12.1 5.1 17 .6z"></path>
             </svg>
-            <p className="text-xl font-semibold mt-4">Frontend</p>
+            <p className="mt-4 text-xl font-semibold">Frontend</p>
           </div>
-          <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
+          <div className="flex-none w-40 p-5 m-4 mx-auto text-center border-2 border-gray-300 rounded-xl">
             <svg
               stroke="currentColor"
               fill="currentColor"
               stroke-width="0"
               viewBox="0 0 640 512"
-              className="text-4xl mx-auto inline-block"
+              className="inline-block mx-auto text-4xl"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M278.9 511.5l-61-17.7c-6.4-1.8-10-8.5-8.2-14.9L346.2 8.7c1.8-6.4 8.5-10 14.9-8.2l61 17.7c6.4 1.8 10 8.5 8.2 14.9L293.8 503.3c-1.9 6.4-8.5 10.1-14.9 8.2zm-114-112.2l43.5-46.4c4.6-4.9 4.3-12.7-.8-17.2L117 256l90.6-79.7c5.1-4.5 5.5-12.3.8-17.2l-43.5-46.4c-4.5-4.8-12.1-5.1-17-.5L3.8 247.2c-5.1 4.7-5.1 12.8 0 17.5l144.1 135.1c4.9 4.6 12.5 4.4 17-.5zm327.2.6l144.1-135.1c5.1-4.7 5.1-12.8 0-17.5L492.1 112.1c-4.8-4.5-12.4-4.3-17 .5L431.6 159c-4.6 4.9-4.3 12.7.8 17.2L523 256l-90.6 79.7c-5.1 4.5-5.5 12.3-.8 17.2l43.5 46.4c4.5 4.9 12.1 5.1 17 .6z"></path>
             </svg>
-            <p className="text-xl font-semibold mt-4">Backend</p>
+            <p className="mt-4 text-xl font-semibold">Backend</p>
           </div>
-          <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
+          <div className="flex-none w-40 p-5 m-4 mx-auto text-center border-2 border-gray-300 rounded-xl">
             <svg
               stroke="currentColor"
               fill="currentColor"
               stroke-width="0"
               viewBox="0 0 512 512"
-              className="text-4xl mx-auto inline-block"
+              className="inline-block mx-auto text-4xl"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M204.3 5C104.9 24.4 24.8 104.3 5.2 203.4c-37 187 131.7 326.4 258.8 306.7 41.2-6.4 61.4-54.6 42.5-91.7-23.1-45.4 9.9-98.4 60.9-98.4h79.7c35.8 0 64.8-29.6 64.9-65.3C511.5 97.1 368.1-26.9 204.3 5zM96 320c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm32-128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128-64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path>
             </svg>
-            <p className="text-xl font-semibold mt-4">Design/UX</p>
+            <p className="mt-4 text-xl font-semibold">Design/UX</p>
           </div>
           <div
-            className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300"
+            className="flex-none w-40 p-5 m-4 mx-auto text-center border-2 border-gray-300 rounded-xl"
             style={{ cursor: "auto" }}
           >
             <svg
@@ -81,7 +80,7 @@ const Main = () => {
               fill="currentColor"
               stroke-width="0"
               viewBox="0 0 512 512"
-              className="text-4xl mx-auto inline-block"
+              className="inline-block mx-auto text-4xl"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
@@ -89,7 +88,7 @@ const Main = () => {
               <path d="M418.2 177.2c-5.4-1.8-10.8-3.5-16.2-5.1.9-3.7 1.7-7.4 2.5-11.1 12.3-59.6 4.2-107.5-23.1-123.3-26.3-15.1-69.2.6-112.6 38.4-4.3 3.7-8.5 7.6-12.5 11.5-2.7-2.6-5.5-5.2-8.3-7.7-45.5-40.4-91.1-57.4-118.4-41.5-26.2 15.2-34 60.3-23 116.7 1.1 5.6 2.3 11.1 3.7 16.7-6.4 1.8-12.7 3.8-18.6 5.9C38.3 196.2 0 225.4 0 255.6c0 31.2 40.8 62.5 96.3 81.5 4.5 1.5 9 3 13.6 4.3-1.5 6-2.8 11.9-4 18-10.5 55.5-2.3 99.5 23.9 114.6 27 15.6 72.4-.4 116.6-39.1 3.5-3.1 7-6.3 10.5-9.7 4.4 4.3 9 8.4 13.6 12.4 42.8 36.8 85.1 51.7 111.2 36.6 27-15.6 35.8-62.9 24.4-120.5-.9-4.4-1.9-8.9-3-13.5 3.2-.9 6.3-1.9 9.4-2.9 57.7-19.1 99.5-50 99.5-81.7 0-30.3-39.4-59.7-93.8-78.4zM282.9 92.3c37.2-32.4 71.9-45.1 87.7-36 16.9 9.7 23.4 48.9 12.8 100.4-.7 3.4-1.4 6.7-2.3 10-22.2-5-44.7-8.6-67.3-10.6-13-18.6-27.2-36.4-42.6-53.1 3.9-3.7 7.7-7.2 11.7-10.7zM167.2 307.5c5.1 8.7 10.3 17.4 15.8 25.9-15.6-1.7-31.1-4.2-46.4-7.5 4.4-14.4 9.9-29.3 16.3-44.5 4.6 8.8 9.3 17.5 14.3 26.1zm-30.3-120.3c14.4-3.2 29.7-5.8 45.6-7.8-5.3 8.3-10.5 16.8-15.4 25.4-4.9 8.5-9.7 17.2-14.2 26-6.3-14.9-11.6-29.5-16-43.6zm27.4 68.9c6.6-13.8 13.8-27.3 21.4-40.6s15.8-26.2 24.4-38.9c15-1.1 30.3-1.7 45.9-1.7s31 .6 45.9 1.7c8.5 12.6 16.6 25.5 24.3 38.7s14.9 26.7 21.7 40.4c-6.7 13.8-13.9 27.4-21.6 40.8-7.6 13.3-15.7 26.2-24.2 39-14.9 1.1-30.4 1.6-46.1 1.6s-30.9-.5-45.6-1.4c-8.7-12.7-16.9-25.7-24.6-39s-14.8-26.8-21.5-40.6zm180.6 51.2c5.1-8.8 9.9-17.7 14.6-26.7 6.4 14.5 12 29.2 16.9 44.3-15.5 3.5-31.2 6.2-47 8 5.4-8.4 10.5-17 15.5-25.6zm14.4-76.5c-4.7-8.8-9.5-17.6-14.5-26.2-4.9-8.5-10-16.9-15.3-25.2 16.1 2 31.5 4.7 45.9 8-4.6 14.8-10 29.2-16.1 43.4zM256.2 118.3c10.5 11.4 20.4 23.4 29.6 35.8-19.8-.9-39.7-.9-59.5 0 9.8-12.9 19.9-24.9 29.9-35.8zM140.2 57c16.8-9.8 54.1 4.2 93.4 39 2.5 2.2 5 4.6 7.6 7-15.5 16.7-29.8 34.5-42.9 53.1-22.6 2-45 5.5-67.2 10.4-1.3-5.1-2.4-10.3-3.5-15.5-9.4-48.4-3.2-84.9 12.6-94zm-24.5 263.6c-4.2-1.2-8.3-2.5-12.4-3.9-21.3-6.7-45.5-17.3-63-31.2-10.1-7-16.9-17.8-18.8-29.9 0-18.3 31.6-41.7 77.2-57.6 5.7-2 11.5-3.8 17.3-5.5 6.8 21.7 15 43 24.5 63.6-9.6 20.9-17.9 42.5-24.8 64.5zm116.6 98c-16.5 15.1-35.6 27.1-56.4 35.3-11.1 5.3-23.9 5.8-35.3 1.3-15.9-9.2-22.5-44.5-13.5-92 1.1-5.6 2.3-11.2 3.7-16.7 22.4 4.8 45 8.1 67.9 9.8 13.2 18.7 27.7 36.6 43.2 53.4-3.2 3.1-6.4 6.1-9.6 8.9zm24.5-24.3c-10.2-11-20.4-23.2-30.3-36.3 9.6.4 19.5.6 29.5.6 10.3 0 20.4-.2 30.4-.7-9.2 12.7-19.1 24.8-29.6 36.4zm130.7 30c-.9 12.2-6.9 23.6-16.5 31.3-15.9 9.2-49.8-2.8-86.4-34.2-4.2-3.6-8.4-7.5-12.7-11.5 15.3-16.9 29.4-34.8 42.2-53.6 22.9-1.9 45.7-5.4 68.2-10.5 1 4.1 1.9 8.2 2.7 12.2 4.9 21.6 5.7 44.1 2.5 66.3zm18.2-107.5c-2.8.9-5.6 1.8-8.5 2.6-7-21.8-15.6-43.1-25.5-63.8 9.6-20.4 17.7-41.4 24.5-62.9 5.2 1.5 10.2 3.1 15 4.7 46.6 16 79.3 39.8 79.3 58 0 19.6-34.9 44.9-84.8 61.4zm-149.7-15c25.3 0 45.8-20.5 45.8-45.8s-20.5-45.8-45.8-45.8c-25.3 0-45.8 20.5-45.8 45.8s20.5 45.8 45.8 45.8z"></path>
             </svg>
             <p
-              className="text-xl font-semibold mt-4"
+              className="mt-4 text-xl font-semibold"
               style={{ cursor: "auto" }}
             >
               ReactJS
@@ -98,18 +97,18 @@ const Main = () => {
         </div>
         <div style={{ cursor: "auto" }}>
           <h1
-            className="mt-8 text-2xl md:text-4xl text-center font-extrabold"
+            className="mt-8 text-2xl font-extrabold text-center md:text-4xl"
             style={{ cursor: "auto" }}
           >
             My projects
           </h1>
           <div
-            className="group w-full sm:w-1/2 m-4 mx-auto p-6 rounded-xl border-2 border-gray-300"
+            className="w-full p-6 m-4 mx-auto border-2 border-gray-300 group sm:w-1/2 rounded-xl"
             style={{ cursor: "auto" }}
           >
             <a href="https://tailwindcss-simplified.netlify.app/">
               <h1
-                className="text-xl text-center font-bold"
+                className="text-xl font-bold text-center"
                 style={{ cursor: "auto" }}
               >
                 Tailwind CSS Playground
@@ -130,7 +129,7 @@ const Main = () => {
             <hr className="my-4" />
             <p className="">👨‍🎨 A Tailwind CSS Playground</p>
             <div
-              className="mt-4 mb-8 flex flex-wrap justify-center items-center gap-2"
+              className="flex flex-wrap items-center justify-center gap-2 mt-4 mb-8"
               style={{ cursor: "auto" }}
             >
               <div className="px-4 py-1 border-2 rounded-full">TailwindCss</div>
@@ -153,12 +152,12 @@ const Main = () => {
             </div>
           </div>
           <div
-            className="group w-full sm:w-1/2 m-4 mx-auto p-6 rounded-xl border-2 border-gray-300"
+            className="w-full p-6 m-4 mx-auto border-2 border-gray-300 group sm:w-1/2 rounded-xl"
             style={{ cursor: "auto" }}
           >
             <a href="https://tailwindcss-builder.netlify.app/">
               <h1
-                className="text-xl text-center font-bold"
+                className="text-xl font-bold text-center"
                 style={{ cursor: "auto" }}
               >
                 TailwindCSS Builder
@@ -181,7 +180,7 @@ const Main = () => {
               ⚡ A tailwind CSS builder to help make webpages
             </p>
             <div
-              className="mt-4 mb-8 flex flex-wrap justify-center items-center gap-2"
+              className="flex flex-wrap items-center justify-center gap-2 mt-4 mb-8"
               style={{ cursor: "auto" }}
             >
               <div className="px-4 py-1 border-2 rounded-full">website</div>
@@ -200,8 +199,8 @@ const Main = () => {
             </div>
           </div>
         </div>
-        <div className="text-center w-full mt-16" style={{ cursor: "auto" }}>
-          <p className="text-gray-600 mb-4" style={{ cursor: "auto" }}>
+        <div className="w-full mt-16 text-center" style={{ cursor: "auto" }}>
+          <p className="mb-4 text-gray-600" style={{ cursor: "auto" }}>
             Made with
             <span role="img" aria-label="heart">
               💙
